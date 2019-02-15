0685ebba98ccd87d705c1a6bfd94543f66dce715
jullynobre <jullynobre1@gmail.com>
2019-02-15 10:46:15 -0300

    Add color to the ball

--- ColorSwitch/ColorSwitch/Scenes/GameScene.swift
+++ ColorSwitch/ColorSwitch/Scenes/GameScene.swift
@@ -8,9 +8,24 @@
 
 import SpriteKit
 
+enum PlayColors {
+    static let colors = [
+        UIColor(red: 231/255, green: 76/255, blue: 60/255, alpha: 1.0),
+        UIColor(red: 241/255, green: 196/255, blue: 15/255, alpha: 1.0),
+        UIColor(red: 46/255, green: 204/255, blue: 113/255, alpha: 1.0),
+        UIColor(red: 52/255, green: 152/255, blue: 219/255, alpha: 1.0)
+    ]
+}
+
+enum SwitchState: Int {
+    case red, yellow, green, blue
+}
+
 class GameScene: SKScene {
     
     var colorSwitch: SKSpriteNode!
+    var switchState = SwitchState.red // 0
+    var currentColorIndex: Int?
     
     override func didMove(to view: SKView) {
         setupPhysics()
@@ -28,7 +43,6 @@ class GameScene: SKScene {
         colorSwitch = SKSpriteNode(imageNamed: "ColorCircle")
         colorSwitch.size = CGSize(width: frame.size.width/3, height: frame.size.width/3)
         colorSwitch.position = CGPoint(x: frame.midX, y: frame.minY + colorSwitch.size.height)
-        
         colorSwitch.physicsBody = SKPhysicsBody(circleOfRadius: colorSwitch.size.width/2)
         colorSwitch.physicsBody?.categoryBitMask = PhysicsCategories.switchCategory
         colorSwitch.physicsBody?.isDynamic = false
@@ -39,10 +53,14 @@ class GameScene: SKScene {
     }
     
     func spawnBall() {
-        let ball = SKSpriteNode(imageNamed: "ball")
-        ball.size = CGSize(width: 30.0, height: 30.0)
-        ball.position = CGPoint(x: frame.midX, y: frame.maxY)
+        currentColorIndex = Int(arc4random_uniform(UInt32(4)))
         
+        let ball = SKSpriteNode(texture: SKTexture(imageNamed: "ball"),
+                                color: PlayColors.colors[currentColorIndex!],
+                                size: CGSize(width: 30.0, height: 30.0))
+        ball.colorBlendFactor = 1.0
+        ball.name = "Ball"
+        ball.position = CGPoint(x: frame.midX, y: frame.maxY)
         ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.height/2)
         ball.physicsBody?.categoryBitMask = PhysicsCategories.ballCategory
         ball.physicsBody?.contactTestBitMask = PhysicsCategories.switchCategory
