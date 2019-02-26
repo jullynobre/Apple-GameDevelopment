c160033c54101a16b4efb444656f099cbb8ef422
jullynobre <jullynobre1@gmail.com>
2019-02-26 08:34:35 -0300

    Format code

--- Fear-The-Dead/Fear-The-Dead/GameScene.swift
+++ Fear-The-Dead/Fear-The-Dead/GameScene.swift
@@ -31,159 +31,158 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    // MARK: - Instance Variables
 
-  // MARK: - Instance Variables
+    let playerSpeed: CGFloat = 150.0
+    let zombieSpeed: CGFloat = 75.0
 
-  let playerSpeed: CGFloat = 150.0
-  let zombieSpeed: CGFloat = 75.0
+    var goal: SKSpriteNode?
+    var player: SKSpriteNode?
+    var zombies: [SKSpriteNode] = []
 
-  var goal: SKSpriteNode?
-  var player: SKSpriteNode?
-  var zombies: [SKSpriteNode] = []
+    var lastTouch: CGPoint?
 
-  var lastTouch: CGPoint? = nil
+    override func didMove(to _: SKView) {
+        // Set up physics world's contact delegate
+        physicsWorld.contactDelegate = self
 
-  override func didMove(to view: SKView) {
-    // Set up physics world's contact delegate
-    physicsWorld.contactDelegate = self
-	
-	// Set up player
-	player = childNode(withName: "player") as? SKSpriteNode
-	
-	// Set up zombies
-	for child in self.children {
-		if child.name == "zombie" {
-			if let child = child as? SKSpriteNode {
-				zombies.append(child)
-			}
-		}
-	}
-	
-	// Set up goal
-	goal = childNode(withName: "goal") as? SKSpriteNode
-	
-    // Set up initial camera position
-    updateCamera()
-  }
+        // Set up player
+        player = childNode(withName: "player") as? SKSpriteNode
 
-  // MARK: - Touch Handling
+        // Set up zombies
+        for child in children {
+            if child.name == "zombie" {
+                if let child = child as? SKSpriteNode {
+                    zombies.append(child)
+                }
+            }
+        }
 
-  override func touchesBegan(_ touches: Set<UITouch>,
-                             with event: UIEvent?) {
-    handleTouches(touches)
-  }
+        // Set up goal
+        goal = childNode(withName: "goal") as? SKSpriteNode
 
-  override func touchesMoved(_ touches: Set<UITouch>,
-                             with event: UIEvent?) {
-    handleTouches(touches)
-  }
-
-  override func touchesEnded(_ touches: Set<UITouch>,
-                             with event: UIEvent?) {
-    handleTouches(touches)
-  }
-
-  fileprivate func handleTouches(_ touches: Set<UITouch>) {
-    lastTouch = touches.first?.location(in: self)
-  }
-
-  override func didSimulatePhysics() {
-    if player != nil {
-      updatePlayer()
-      updateZombies()
+        // Set up initial camera position
+        updateCamera()
     }
-  }
 
-  // Determines whether the player's position should be updated
-  fileprivate func shouldMove(currentPosition: CGPoint,
-                              touchPosition: CGPoint) -> Bool {
-    guard let player = player else { return false }
-    return abs(currentPosition.x - touchPosition.x) > player.frame.width / 2 ||
-      abs(currentPosition.y - touchPosition.y) > player.frame.height / 2
-  }
+    // MARK: - Touch Handling
 
-  fileprivate func updatePlayer() {
-    guard let player = player,
-    let touch = lastTouch
-      else { return }
-    let currentPosition = player.position
-    if shouldMove(currentPosition: currentPosition,
-                  touchPosition: touch) {
-      updatePosition(for: player, to: touch, speed: playerSpeed)
-      updateCamera()
-    } else {
-      player.physicsBody?.isResting = true
+    override func touchesBegan(_ touches: Set<UITouch>,
+                               with _: UIEvent?) {
+        handleTouches(touches)
     }
-  }
 
-  fileprivate func updateCamera() {
-    guard let player = player else { return }
-    camera?.position = player.position
-  }
-
-  // Updates the position of all zombies by moving towards the player
-  func updateZombies() {
-    guard let player = player else { return }
-    let targetPosition = player.position
-
-    for zombie in zombies {
-      updatePosition(for: zombie, to: targetPosition, speed: zombieSpeed)
+    override func touchesMoved(_ touches: Set<UITouch>,
+                               with _: UIEvent?) {
+        handleTouches(touches)
     }
-  }
 
-  fileprivate func updatePosition(for sprite: SKSpriteNode,
-                                  to target: CGPoint,
-                                  speed: CGFloat) {
-    let currentPosition = sprite.position
-    let angle = CGFloat.pi + atan2(currentPosition.y - target.y,
-                                   currentPosition.x - target.x)
-    let rotateAction = SKAction.rotate(toAngle: angle + (CGFloat.pi*0.5),
-                                       duration: 0)
-    sprite.run(rotateAction)
+    override func touchesEnded(_ touches: Set<UITouch>,
+                               with _: UIEvent?) {
+        handleTouches(touches)
+    }
 
-    let velocityX = speed * cos(angle)
-    let velocityY = speed * sin(angle)
+    fileprivate func handleTouches(_ touches: Set<UITouch>) {
+        lastTouch = touches.first?.location(in: self)
+    }
 
-    let newVelocity = CGVector(dx: velocityX, dy: velocityY)
-    sprite.physicsBody?.velocity = newVelocity
-  }
+    override func didSimulatePhysics() {
+        if player != nil {
+            updatePlayer()
+            updateZombies()
+        }
+    }
+
+    // Determines whether the player's position should be updated
+    fileprivate func shouldMove(currentPosition: CGPoint,
+                                touchPosition: CGPoint) -> Bool {
+        guard let player = player else { return false }
+        return abs(currentPosition.x - touchPosition.x) > player.frame.width / 2 ||
+            abs(currentPosition.y - touchPosition.y) > player.frame.height / 2
+    }
+
+    fileprivate func updatePlayer() {
+        guard let player = player,
+            let touch = lastTouch
+        else { return }
+        let currentPosition = player.position
+        if shouldMove(currentPosition: currentPosition,
+                      touchPosition: touch) {
+            updatePosition(for: player, to: touch, speed: playerSpeed)
+            updateCamera()
+        } else {
+            player.physicsBody?.isResting = true
+        }
+    }
+
+    fileprivate func updateCamera() {
+        guard let player = player else { return }
+        camera?.position = player.position
+    }
+
+    // Updates the position of all zombies by moving towards the player
+    func updateZombies() {
+        guard let player = player else { return }
+        let targetPosition = player.position
+
+        for zombie in zombies {
+            updatePosition(for: zombie, to: targetPosition, speed: zombieSpeed)
+        }
+    }
+
+    fileprivate func updatePosition(for sprite: SKSpriteNode,
+                                    to target: CGPoint,
+                                    speed: CGFloat) {
+        let currentPosition = sprite.position
+        let angle = CGFloat.pi + atan2(currentPosition.y - target.y,
+                                       currentPosition.x - target.x)
+        let rotateAction = SKAction.rotate(toAngle: angle + (CGFloat.pi * 0.5),
+                                           duration: 0)
+        sprite.run(rotateAction)
+
+        let velocityX = speed * cos(angle)
+        let velocityY = speed * sin(angle)
+
+        let newVelocity = CGVector(dx: velocityX, dy: velocityY)
+        sprite.physicsBody?.velocity = newVelocity
+    }
 }
 
 // MARK: - SKPhysicsContactDelegate
 
 extension GameScene: SKPhysicsContactDelegate {
-  func didBegin(_ contact: SKPhysicsContact) {
-    // 1. Create local variables for two physics bodies
-    var firstBody: SKPhysicsBody
-    var secondBody: SKPhysicsBody
+    func didBegin(_ contact: SKPhysicsContact) {
+        // 1. Create local variables for two physics bodies
+        var firstBody: SKPhysicsBody
+        var secondBody: SKPhysicsBody
 
-    // 2. Assign the two physics bodies so that the one with the
-    // lower category is always stored in firstBody
-    if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
-      firstBody = contact.bodyA
-      secondBody = contact.bodyB
-    } else {
-      firstBody = contact.bodyB
-      secondBody = contact.bodyA
+        // 2. Assign the two physics bodies so that the one with the
+        // lower category is always stored in firstBody
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            firstBody = contact.bodyA
+            secondBody = contact.bodyB
+        } else {
+            firstBody = contact.bodyB
+            secondBody = contact.bodyA
+        }
+
+        // 3. react to the contact between the two nodes
+        if firstBody.categoryBitMask == player?.physicsBody?.categoryBitMask,
+            secondBody.categoryBitMask == zombies[0].physicsBody?.categoryBitMask {
+            // Player & Zombie
+            gameOver(false)
+        } else if firstBody.categoryBitMask == player?.physicsBody?.categoryBitMask,
+            secondBody.categoryBitMask == goal?.physicsBody?.categoryBitMask {
+            // Player & Goal
+            gameOver(true)
+        }
     }
 
-    // 3. react to the contact between the two nodes
-    if firstBody.categoryBitMask == player?.physicsBody?.categoryBitMask &&
-      secondBody.categoryBitMask == zombies[0].physicsBody?.categoryBitMask {
-      // Player & Zombie
-      gameOver(false)
-    } else if firstBody.categoryBitMask == player?.physicsBody?.categoryBitMask &&
-      secondBody.categoryBitMask == goal?.physicsBody?.categoryBitMask {
-      // Player & Goal
-      gameOver(true)
+    // MARK: - Helper Functions
+
+    fileprivate func gameOver(_ didWin: Bool) {
+        let menuScene = MenuScene(size: size, didWin: didWin)
+        let transition = SKTransition.flipVertical(withDuration: 1.0)
+        view?.presentScene(menuScene, transition: transition)
     }
-  }
-
-  // MARK: - Helper Functions
-
-  fileprivate func gameOver(_ didWin: Bool) {
-    let menuScene = MenuScene(size: size, didWin: didWin)
-    let transition = SKTransition.flipVertical(withDuration: 1.0)
-    view?.presentScene(menuScene, transition: transition)
-  }
 }
--- Fear-The-Dead/Fear-The-Dead/MenuScene.swift
+++ Fear-The-Dead/Fear-The-Dead/MenuScene.swift
@@ -31,49 +31,48 @@
 import SpriteKit
 
 class MenuScene: SKScene {
-  var didWin: Bool
+    var didWin: Bool
 
-  required init?(coder aDecoder: NSCoder) {
-    fatalError()
-  }
-
-  init(size: CGSize, didWin: Bool) {
-    self.didWin = didWin
-    super.init(size: size)
-    scaleMode = .aspectFill
-  }
-
-  override func didMove(to view: SKView) {
-    backgroundColor = SKColor(white: 0, alpha: 1)
-
-    // Set up labels
-    let text = didWin ? "You Won!!!" : "You lost :["
-    let winLabel = SKLabelNode(text: text)
-    winLabel.fontName = "AvenirNext-Bold"
-    winLabel.fontSize = 65
-    winLabel.fontColor = .white
-    winLabel.position = CGPoint(x: frame.midX, y: frame.midY*1.5)
-    addChild(winLabel)
-
-    let label = SKLabelNode(text: "Press anywhere to play again!")
-    label.fontName = "AvenirNext-Bold"
-    label.fontSize = 55
-    label.fontColor = .white
-    label.position = CGPoint(x: frame.midX, y: frame.midY)
-    addChild(label)
-
-    // Play sound
-    let soundToPlay = didWin ? "fear_win.mp3" : "fear_lose.mp3"
-    run(SKAction.playSoundFileNamed(soundToPlay, waitForCompletion: false))
-
-  }
-
-  override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-    guard let gameScene = GameScene(fileNamed: "GameScene") else {
-      fatalError("GameScene not found")
+    required init?(coder _: NSCoder) {
+        fatalError()
     }
-    let transition = SKTransition.flipVertical(withDuration: 1.0)
-    gameScene.scaleMode = .aspectFill
-    view?.presentScene(gameScene, transition: transition)
-  }
+
+    init(size: CGSize, didWin: Bool) {
+        self.didWin = didWin
+        super.init(size: size)
+        scaleMode = .aspectFill
+    }
+
+    override func didMove(to _: SKView) {
+        backgroundColor = SKColor(white: 0, alpha: 1)
+
+        // Set up labels
+        let text = didWin ? "You Won!!!" : "You lost :["
+        let winLabel = SKLabelNode(text: text)
+        winLabel.fontName = "AvenirNext-Bold"
+        winLabel.fontSize = 65
+        winLabel.fontColor = .white
+        winLabel.position = CGPoint(x: frame.midX, y: frame.midY * 1.5)
+        addChild(winLabel)
+
+        let label = SKLabelNode(text: "Press anywhere to play again!")
+        label.fontName = "AvenirNext-Bold"
+        label.fontSize = 55
+        label.fontColor = .white
+        label.position = CGPoint(x: frame.midX, y: frame.midY)
+        addChild(label)
+
+        // Play sound
+        let soundToPlay = didWin ? "fear_win.mp3" : "fear_lose.mp3"
+        run(SKAction.playSoundFileNamed(soundToPlay, waitForCompletion: false))
+    }
+
+    override func touchesEnded(_: Set<UITouch>, with _: UIEvent?) {
+        guard let gameScene = GameScene(fileNamed: "GameScene") else {
+            fatalError("GameScene not found")
+        }
+        let transition = SKTransition.flipVertical(withDuration: 1.0)
+        gameScene.scaleMode = .aspectFill
+        view?.presentScene(gameScene, transition: transition)
+    }
 }
